95189726b55dfd9a80c29cf9c6527931645f614d
merveille-dona <149607747+merveille-dona@users.noreply.github.com>
2025-08-11 13:14:09 +0200

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,124 @@
+const themes = {
+    blue: {
+        light: {
+            "--primary-color": "rgb(0, 102, 255)",
+            "--background-color": "#f5f5f5",
+            "--text-color": "#121212",
+        },
+        dark: {
+            "--primary-color": "rgb(15, 15, 103)",
+            "--background-color": "#121212",
+            "--text-color": "#F5F5F5",
+        },
+    },
+    red: {
+        light: {
+            "--primary-color": "rgb(220, 20, 60)",
+            "--background-color": "#f5f5f5",
+            "--text-color": "#121212",
+        },
+        dark: {
+            "--primary-color": "rgb(139, 0, 0)",
+            "--background-color": "#121212",
+            "--text-color": "#F5F5F5",
+        },
+    },
+    green: {
+        light: {
+            "--primary-color": "rgb(34, 139, 34)",
+            "--background-color": "#f5f5f5",
+            "--text-color": "#121212",
+        },
+        dark: {
+            "--primary-color": "rgb(0, 100, 0)",
+            "--background-color": "#121212",
+            "--text-color": "#F5F5F5",
+        },
+    },
+    violet: {
+        light: {
+            "--primary-color": "rgb(138, 43, 226)",
+            "--background-color": "#f5f5f5",
+            "--text-color": "#121212",
+        },
+        dark: {
+            "--primary-color": "rgb(75, 0, 130)",
+            "--background-color": "#121212",
+            "--text-color": "#F5F5F5",
+        },
+    },
+    grey: {
+        light: {
+            "--primary-color": "rgb(169, 169, 169)",
+            "--background-color": "#f5f5f5",
+            "--text-color": "#121212",
+        },
+        dark: {
+            "--primary-color": "rgb(105, 105, 105)",
+            "--background-color": "#121212",
+            "--text-color": "#F5F5F5",
+        },
+    },
+    deepYellow: {
+        light: {
+            "--primary-color": "rgb(255, 193, 7)",
+            "--background-color": "#f5f5f5",
+            "--text-color": "#121212",
+        },
+        dark: {
+            "--primary-color": "rgb(204, 140, 0)",
+            "--background-color": "#121212",
+            "--text-color": "#F5F5F5",
+        },
+    },
+    "orange": {
+        "light": {
+            "--primary-color": "rgb(255, 87, 34)",
+            "--background-color": "#f5f5f5",
+            "--text-color": "#121212"
+        },
+        "dark": {
+            "--primary-color": "rgb(230, 74, 25)",
+            "--background-color": "#121212",
+            "--text-color": "#F5F5F5"
+        }
+    },
+    "beige": {
+        "light": {
+            "--primary-color": "rgb(214, 188, 155)",
+            "--background-color": "#faf3e0",
+            "--text-color": "#121212"
+        },
+        "dark": {
+            "--primary-color": "rgb(191, 162, 126)",
+            "--background-color": "#121212",
+            "--text-color": "#F5F5F5"
+        }
+    },
+    "magenta": {
+        "light": {
+            "--primary-color": "rgb(233, 30, 99)",
+            "--background-color": "#f5f5f5",
+            "--text-color": "#121212"
+        },
+        "dark": {
+            "--primary-color": "rgb(194, 24, 91)",
+            "--background-color": "#121212",
+            "--text-color": "#F5F5F5"
+        }
+    },
+    "blackGrey": {
+        "light": {
+            "--primary-color": "#000000",
+            "--background-color": "#FFFFFF",
+            "--text-color": "#121212"
+        },
+        "dark": {
+            "--primary-color": "rgb(173, 173, 173",
+            "--background-color": "#121212",
+            "--text-color": "#F5F5F5"
+        }
+    }
+}
+
+export default themes
